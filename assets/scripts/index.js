a39b5fa3bdb6155dc4ebba94309c15a72807b2b3
ian-iglesias <ianiglesias6798@gmail.com>
2022-12-25 00:21:02 -0300

    adição de imagens da primeira e segunda section.

---
+++ assets/scripts/index.js
@@ -0,0 +1,104 @@
+const greenSnake= {
+    name: "Green Snake",
+    symbol: "Earth",
+    level: 4,
+    attack: 200,
+    defense: 300,
+    rarity: "Common"
+};
+
+const blueSnake = {
+    name: "Blue Snake",
+    symbol: "Water",
+    level: 4,
+    attack: 200,
+    defense: 300,
+    rarity: "Common"
+};
+
+const redSnake = {
+    name: "Red Snake",
+    symbol: "Fire",
+    level: 4,
+    attack: 200,
+    defense: 300,
+    rarity: "Common"
+};
+
+const darkSnake = {
+    name: "Dark Snake",
+    symbol: "Dark",
+    level: 4,
+    attack: 200,
+    defense: 300,
+    rarity: "Common"
+};
+
+const lightSnake = {
+    name: "Light Snake",
+    symbol: "Light",
+    level: 4,
+    attack: 200,
+    defense: 300,
+    rarity: "Common"
+};
+
+const greenEyedGreenSnake = {
+    name: "Green-Eyed Green Snake",
+    symbol: "Earth",
+    level: 5,
+    attack: 300,
+    defense: 300,
+    rarity: "Rare"
+};
+
+const greenEyedBlueSnake = {
+    name: "Green-Eyed Blue Snake",
+    symbol: "Water",
+    level: 5,
+    attack: 300,
+    defense: 300,
+    rarity: "Rare"
+};
+
+const greenEyedRedSnake = {
+    name: "Green-Eyed Red Snake",
+    symbol: "Fire",
+    level: 5,
+    attack: 300,
+    defense: 300,
+    rarity: "Rare"
+};
+
+const greenEyedDarkSnake = {
+    name: "Green-Eyed Dark Snake",
+    symbol: "Dark",
+    level: 5,
+    attack: 300,
+    defense: 300,
+    rarity: "Rare"
+};
+
+const greenEyedLightSnake = {
+    name: "Green-Eyed Light Snake",
+    symbol: "Light",
+    level: 5,
+    attack: 300,
+    defense: 300,
+    rarity: "Rare"
+};
+
+const greenPaintedSnake = {
+    name: "Green Painted Snake",
+    symbol: "Earth",
+    level: 7,
+    attack: 600,
+    defense: 700,
+    rarity: "Ultra-rare"
+};
+
+const commonSnakes = [greenSnake, blueSnake, redSnake, darkSnake, lightSnake];
+
+const rareSnakes = [greenEyedGreenSnake, greenEyedBlueSnake, greenEyedRedSnake, greenEyedDarkSnake, greenEyedLightSnake];
+
+const ultraRareSnakes = [greenPaintedSnake];
